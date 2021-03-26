f1ef779fb0bce4c97028d3d3d6e41d195cd1b14b
rmezied <rmezied3@gmail.com>
2021-03-26 06:33:41 -0700

    apdate general styles for todo app relates #5

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function add(text) {
 
     let containerDiv = document.createElement("div")
     containerDiv.setAttribute("class","container-div")
-<<<<<<< HEAD
 
     let subDiv1 = document.createElement("div")
     subDiv1.setAttribute("class","sub-div1")
@@ -41,20 +40,6 @@ function add(text) {
     let del = document.createTextNode("delete");
 
 
-=======
-    let subDiv1 = document.createElement("div")
-    subDiv1.setAttribute("class","sub-div1")
-    let subDiv2 = document.createElement("div")
-    subDiv2.setAttribute("class","sub-div2")
-    let toDo = document.createElement("p")
-    toDo.setAttribute("class","paragraph")
-    let subButton1 = document.createElement("button")
-    subButton1.setAttribute("class","bt1")
-    let subButton2 = document.createElement("button")
-    subButton2.setAttribute("class","bt2")
-    let containerText = document.createTextNode(text);
-    let del = document.createTextNode("delete");
->>>>>>> 9a3ed1b138472fc8bb129f9e3dd5b1155a052308
     subButton1.setAttribute("onclick","del(event)")
     subButton2.setAttribute("onclick","edit(event)")
     let edit = document.createTextNode("edit");
@@ -67,12 +52,7 @@ function add(text) {
     subDiv2.appendChild(subButton1)
     subDiv2.appendChild(subButton2)
     document.querySelector(".app-container").appendChild(containerDiv)
-<<<<<<< HEAD
    }
-=======
-   }   
-   
->>>>>>> 9a3ed1b138472fc8bb129f9e3dd5b1155a052308
 itemsArray.forEach(function(item){
     add(item)
 });
